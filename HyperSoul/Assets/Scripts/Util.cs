0b6cd4e0f64cfcb57e69ffc52553e85cbb571b9e
WoongjuChoi <dndwn1125@gmail.com>
2022-03-21 21:30:45 +0900

    Feat : Player 이동 구현

---
+++ HyperSoul/Assets/Scripts/Util.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+class InputParameterID
+{
+    public const string VERTICAL = "Vertical";
+    public const string HORIZONTAL = "Horizontal";
+}
+
+class PlayerAnimatorID
+{
+    public static readonly int VERTICAL = Animator.StringToHash(InputParameterID.VERTICAL);
+    public static readonly int HORIZONTAL = Animator.StringToHash(InputParameterID.HORIZONTAL);
+}

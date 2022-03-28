8486c27d16e207783590970e9d8d2465ba59a2f6
Zziroo <tlslrs1994@naver.com>
2022-03-28 13:05:46 +0900

    Feat : Add Monster idle state 몬스터 대기 상태 추가

--- HyperSoul/Assets/Scripts/FSM/Monster States/MonsterIdleState.cs
+++ HyperSoul/Assets/Scripts/FSM/Monster States/MonsterIdleState.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class MonsterIdleState : IfiniteState
 {
+    private GameObject _gameObject = null;
+
+    private MonsterInfomations _monsterInfo = null;
+
     private FiniteStateMachine _finiteStateMachine = null;
 
     public void EnterState()
     {
-        Debug.Log("MonsterIdleState EnterState");
+        _monsterInfo = _gameObject.GetComponent<MonsterInfomations>();
+
+        _monsterInfo.MonsterCurrentState = EStateIDs.Idle;
+
+        _gameObject.GetComponentInChildren<Animator>().SetTrigger(MonsterAnimatorID.HAS_IDLE);
     }
 
     public void ExitState()
     {
-        Debug.Log("MonsterIdleState ExitState");
     }
 
     public void InitializeState(GameObject obj, FiniteStateMachine fsm)
     {
-        _finiteStateMachine = fsm;
+        _gameObject = obj;
 
-        Debug.Log("MonsterIdleState InitializeState");
+        _finiteStateMachine = fsm;
     }
 
     public void UpdateState()
     {
-        Debug.Log("MonsterIdleState UpdateState");
+        if (_monsterInfo.IsDamaged)
+        {
+            _finiteStateMachine.ChangeState(EStateIDs.Damaged);
+        }
     }
 }
--- HyperSoul/Assets/Scripts/GameObject/Monster/MonsterInfomations.cs
+++ HyperSoul/Assets/Scripts/GameObject/Monster/MonsterInfomations.cs
@@ -16,19 +16,28 @@ public class MonsterInfomations : MonoBehaviour
     private MonsterAlertState _monsterAlertState = new MonsterAlertState();
     private MonsterAttackState _monsterAttackState = new MonsterAttackState();
     private MonsterChaseState _monsterChaseState = new MonsterChaseState();
+    private MonsterDamagedState _monsterDamagedState = new MonsterDamagedState();
     private MonsterDieState _monsterDieState = new MonsterDieState();
     private MonsterIdleState _monsterIdleState = new MonsterIdleState();
     private MonsterReturnPositionState _monsterReturnPositionState = new MonsterReturnPositionState();
     private MonsterSpawnState _monsterSpawnState = new MonsterSpawnState();
 
+    private bool _isWithinAttackRange = false;
+
+    public GameObject Target = null;
+    public EStateIDs MonsterCurrentState { get; set; }
+    public bool IsDamaged { get; set; }
+
     public float MonsterInvincibleTime { get { return _monsterInvincibleTime; } }
     public int MonsterHP { get { return _monsterHP; } }
+    public bool IsWithinAttackRange { get { return _isWithinAttackRange; } }
 
     private void Awake()
     {
         _monsterFSM.AddState(EStateIDs.Alert, _monsterAlertState);
         _monsterFSM.AddState(EStateIDs.Attack, _monsterAttackState);
         _monsterFSM.AddState(EStateIDs.Chase, _monsterChaseState);
+        _monsterFSM.AddState(EStateIDs.Damaged, _monsterDamagedState);
         _monsterFSM.AddState(EStateIDs.Die, _monsterDieState);
         _monsterFSM.AddState(EStateIDs.Idle, _monsterIdleState);
         _monsterFSM.AddState(EStateIDs.ReturnPosition, _monsterReturnPositionState);
@@ -37,6 +46,36 @@ public class MonsterInfomations : MonoBehaviour
 
     private void OnEnable()
     {
-        _monsterFSM.InitializeState(EStateIDs.Spawn);
+        _monsterFSM.ChangeState(EStateIDs.Spawn);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (EStateIDs.Damaged == MonsterCurrentState || EStateIDs.Chase == MonsterCurrentState)
+        {
+            _isWithinAttackRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (EStateIDs.Damaged == MonsterCurrentState || EStateIDs.Attack == MonsterCurrentState)
+        {
+            _isWithinAttackRange = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (false == IsDamaged && EStateIDs.Idle == MonsterCurrentState)
+        {
+            IsDamaged = true;
+
+            Target = collision.gameObject.GetComponent<SampleAmmo>().Owner;
+        }
+    }
+
+    private void Update()
+    {
     }
 }

63c2f4521ef2600f6802939533614f93b5be14d3
Zziroo <tlslrs1994@naver.com>
2022-03-29 17:52:17 +0900

    Feat : Add Monster chase state 몬스터 추격 상태 추가

--- HyperSoul/Assets/Scripts/FSM/Monster States/MonsterChaseState.cs
+++ HyperSoul/Assets/Scripts/FSM/Monster States/MonsterChaseState.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class MonsterChaseState : IfiniteState
 {
+    private GameObject _gameObject = null;
+
+    private MonsterInfomations _monsterInfo = null;
+
     private FiniteStateMachine _finiteStateMachine = null;
 
     public void EnterState()
     {
+        _monsterInfo = _gameObject.GetComponent<MonsterInfomations>();
+
+        _monsterInfo.MonsterCurrentState = EStateIDs.Chase;
+
+        _gameObject.GetComponentInChildren<Animator>().SetTrigger(MonsterAnimatorID.HAS_CHASE);
     }
 
     public void ExitState()
     {
+        _monsterInfo.MonsterChaser.IsActive = false;
+
+        _monsterInfo.MonsterChaser.ResetPath();
     }
 
     public void InitializeState(GameObject obj, FiniteStateMachine fsm)
     {
+        _gameObject = obj;
+
         _finiteStateMachine = fsm;
     }
 
     public void UpdateState()
     {
+        if (_monsterInfo.IsDamaged)
+        {
+            _finiteStateMachine.ChangeState(EStateIDs.Damaged);
+
+            return;
+        }
+
+        if (_monsterInfo.IsWithinAttackRange)
+        {
+            _finiteStateMachine.ChangeState(EStateIDs.Attack);
+
+            return;
+        }
+
+        _monsterInfo.MonsterChaser.IsActive = true;
     }
 }

c320caf2f310aeb6d7ad2e1a409f176d0e4c73c8
WoongjuChoi <dndwn1125@gmail.com>
2022-04-10 22:10:47 +0900

    Chore : chore

--- HyperSoul/Assets/Scripts/Manager/GameManager.cs
+++ HyperSoul/Assets/Scripts/Manager/GameManager.cs
@@ -65,8 +65,8 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             index = Random.Range(1, 5);
         }
-        //GameObject obj = PhotonNetwork.Instantiate("BazookaPlayer", spawnPoint[index].position, Quaternion.identity);
-        GameObject obj = PhotonNetwork.Instantiate("RiflePlayer", spawnPoint[index].position, Quaternion.identity);
+        GameObject obj = PhotonNetwork.Instantiate("BazookaPlayer", spawnPoint[index].position, Quaternion.identity);
+        //GameObject obj = PhotonNetwork.Instantiate("RiflePlayer", spawnPoint[index].position, Quaternion.identity);
         //GameObject obj = PhotonNetwork.Instantiate("SniperPlayer", spawnPoint[index].position, Quaternion.identity);
 
         photonView.RPC("SpawnPosMarking", RpcTarget.AllBuffered, index);
